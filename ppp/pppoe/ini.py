299160c727b037676803a0d60652f85e5497848b
knknkn1162 <knknkn1162@gmail.com>
2024-06-15 11:45:21 +0900

    pppoe

--- ppp/pppoe/ini.py
+++ ppp/pppoe/ini.py
@@ -1,12 +1,9 @@
 SUBNET_MASK_24 = "255.255.255.0"
 INVERSE_MASK_24 = "0.0.0.255"
-pcap_file = "test.pcap"
-channel_group=1
-ETHERCHANNEL_LACP='lacp'
+chap_password = "example109324"
 
 class r1:
-  username = "r1pap"
-  password = "example20934"
+  username = "r1chap"
   class g0_0:
     name = "FastEthernet0/0"
     ip_addr = "192.168.0.254"
@@ -17,8 +14,7 @@ class r1:
     subnet_mask = SUBNET_MASK_24
 
 class r2:
-  username = "r2pap"
-  password = "example20935"
+  username = "r2chap"
   class g0_0:
     name = "FastEthernet0/0"
     ip_addr = "192.168.1.254"
